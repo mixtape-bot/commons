54061e2406ef39e16fc601ed7be2781c8cdc0e1d
melike2d <gino@dimensional.fun>
2021-04-19 03:58:38 -0700

    :adhesive_bandage: use per-factory thread counter

--- src/main/kotlin/mixtape/commons/ThreadFactory.kt
+++ src/main/kotlin/mixtape/commons/ThreadFactory.kt
@@ -4,17 +4,18 @@ import java.util.*
 import java.util.concurrent.ThreadFactory
 import java.util.concurrent.atomic.AtomicInteger
 
-val counter = AtomicInteger()
 fun threadFactory(
   name: String,
   daemon: Boolean? = null,
   priority: Int? = null,
   exceptionHandler: Thread.UncaughtExceptionHandler? = null
-) =
-  ThreadFactory { runnable ->
+): ThreadFactory {
+  val counter = AtomicInteger()
+  return ThreadFactory { runnable ->
     Thread(runnable, name.format(Locale.ROOT, counter.getAndIncrement())).apply {
       daemon?.let { this.isDaemon = it }
       priority?.let { this.priority = priority }
       exceptionHandler?.let { this.uncaughtExceptionHandler = it }
     }
   }
+}
