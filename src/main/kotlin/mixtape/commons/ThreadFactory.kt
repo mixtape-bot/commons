9d5f32321cf3e496aee89856f219f67dbebbafc8
melike2d <gino@dimensional.fun>
2021-04-18 23:31:29 -0700

    :sparkles: thread factory util

---
+++ src/main/kotlin/mixtape/commons/ThreadFactory.kt
@@ -0,0 +1,20 @@
+package mixtape.commons
+
+import java.util.*
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.atomic.AtomicInteger
+
+val counter = AtomicInteger()
+fun threadFactory(
+  name: String,
+  daemon: Boolean? = null,
+  priority: Int? = null,
+  exceptionHandler: Thread.UncaughtExceptionHandler? = null
+) =
+  ThreadFactory { runnable ->
+    Thread(runnable, name.format(Locale.ROOT, counter.getAndIncrement())).apply {
+      daemon?.let { this.isDaemon = it }
+      priority?.let { this.priority = priority }
+      exceptionHandler?.let { this.uncaughtExceptionHandler = it }
+    }
+  }
